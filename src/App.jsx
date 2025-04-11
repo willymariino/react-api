504fb485b5ac0037d3e9ad761068804ac263e008
willymariino <wg.mariino@gmail.com>
2025-04-11 17:17:02 +0200

    axios.get

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,15 @@ import axios from 'axios'
 function App() {
   const [actors, setActors] = useState([])
 
+  // fetching dei dati
+
+  function actors() {
+    axios.get("https://freetestapi.com/api/v1/actors")
+      .then((res) => setActors(res.data))
+
+
+  }
+
   return (
     <>
 
