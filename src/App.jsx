c585f9c1e5053e27e7e216b74775eb8e2a009820
willymariino <wg.mariino@gmail.com>
2025-04-12 17:39:15 +0200

    added comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,17 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+
+export default function App() {
+  const [actors, setActors] = useState([]) /* creazione dello stato: creaimo una lista in cui conserveremo la lista degli actors,
+                                              utilizziamo los tato per aggiornare dinamicamentela UI quando otteniamo i dati dal server. */
+
+  function fetchActors() {
+
+
+  }
+
+
+
+}
+
+
