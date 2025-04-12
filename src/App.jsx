76429f2bc545ae1edda9b544d9aa3a9ce6ab0b2b
willymariino <wg.mariino@gmail.com>
2025-04-12 17:56:20 +0200

    added comment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ export default function App() {
                                               utilizziamo los tato per aggiornare dinamicamentela UI quando otteniamo i dati dal server. */
 
   function fetchActors() {
+    axios.get("https://www.freetestapi.com/api/v1/actors") // chaiamata API in get
+      .then((res) => setActors(res.data))
+      .catch((error) => console.error("errore durante la chiamata API", error))
 
+    /* res rappresenta la risposta dell'API, res.data: contiene i dati principali restituiti dall'API (JSON)
+    setActors aggiorna lo stato con i dati ricevuti,
+    il then consente la gestiione asincrona, eseguendo il codice solo dopo che l'operazione è stata completata */
 
   }
 
