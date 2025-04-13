6c8761acd0d0956155a8df299652192c85e6fac6
willymariino <wg.mariino@gmail.com>
2025-04-13 15:23:58 +0200

    added card component

--- src/components/actorCard.jsx
+++ src/components/actorCard.jsx
@@ -1,7 +1,16 @@
 function actorCard({ name, image }) {
 
     return (
-        <div  >
+        <div className="container-card-items" >
+            <h1>Attori famosi</h1>
+
+            {actors.map((actor) => (
+                <actorCard key={actor.id} />
+            )
+
+
+
+            )}
 
 
 
