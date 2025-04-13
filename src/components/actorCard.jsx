51bbe2a568054fdf0d04adb42e95e60d93bfc127
willymariino <wg.mariino@gmail.com>
2025-04-13 14:55:01 +0200

    cleaned up the return

--- src/components/actorCard.jsx
+++ src/components/actorCard.jsx
@@ -1,30 +1,9 @@
 function actorCard({ name, image }) {
 
     return (
-        <div className="card">
-            <div className="image-container">
-                <img src={actor.image} alt={actor.name} />
-            </div>
-
-            <div className="textbox">
-                <p>nome: {actor.name}</p>
-                <p> anno di nascita: {actor.birth_year}</p>
-                <p>morte: {actor.death_year}</p>
-                <p>nazionalità: {actor.nationality}</p>
-
-                <p> conosciuto per: </p>
-                <ul>
-                    {actor.known_for.map((movie, index) => (
-                        <li key={index}>{movie}</li>
-                    )
+        <div  >
 
 
-                    )}
-
-
-                </ul>
-
-            </div>
 
         </div>
     )
